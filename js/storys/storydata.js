a7754fcae83393563b420c55b410323ecf29361f
xli252 <xiu-jie.li@dxc.com>
2024-06-28 10:36:50 +0800

    英语读

---
+++ js/storys/storydata.js
@@ -0,0 +1,15 @@
+var wordList = [
+    {
+        "reading": [
+            "Once upon a time there was an old mother pig who had three little pigs and not enough food to feed them. So when they were old enough, she sent them out into the world to seek their fortunes.",
+            "The first little pig was very lazy. He didn't want to work at all and he built his house out of straw. The second little pig worked a little bit harder but he was somewhat lazy too and he built his house out of sticks. Then, they sang and danced and played together the rest of the day.",
+            "The third little pig worked hard all day and built his house with bricks. It was a sturdy house complete with a fine fireplace and chimney. It looked like it could withstand the strongest winds.",
+            // "Then I'll huff",
+            // "and I'll puff ",
+            // "and I'll blow your house down.",
+            // "So he huffed and he puffed and he blew the house down! The wolf opened his jaws very wide and bit down as hard as he could, but the first little pig escaped and ran away to hide with the second little pig.",
+            // "The wolf continued down the lane and he passed by the second house made of sticks; and he saw the house, and he smelled the pigs inside, and his mouth began to water as he thought about the fine dinner they would make.",
+            // ""
+        ]
+    }
+]

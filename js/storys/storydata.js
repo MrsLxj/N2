bf0fc2145aad8e5bf1723fb1fdcb6f45e1765249
MrsLxj <1404942787@qq.com>
2024-09-29 17:32:46 +0800

    Update storydata.js

--- js/storys/storydata.js
+++ js/storys/storydata.js
@@ -1,9 +1,19 @@
 var wordList = [
     {
         "reading": [
-            "Once upon a time there was an old mother pig who had three little pigs and not enough food to feed them. So when they were old enough, she sent them out into the world to seek their fortunes.",
-            "The first little pig was very lazy. He didn't want to work at all and he built his house out of straw. The second little pig worked a little bit harder but he was somewhat lazy too and he built his house out of sticks. Then, they sang and danced and played together the rest of the day.",
-            "The third little pig worked hard all day and built his house with bricks. It was a sturdy house complete with a fine fireplace and chimney. It looked like it could withstand the strongest winds.",
+
+            "初めまして、簡単に自己紹介をさせていただきます。私は李秀傑と申します。33歳で、大連湾に住んでいます。今日は、貴社の面接に参加させていただき、非常に光栄に思います。",
+            "卒業以来、10年間、システム开発に従事しました。プログラマーとして、言語はドットネットで主に开発経験があります。SQL、HTML、js、css、Vueなど熟練度も高いです。プログラムだけではなく、お客様と認識が合って、設計書の修正や製造や単体試験を経験しております。その他、業務を円滑に進めるために、ツールにおいて、EXCELVBAを作った経験があります。",
+            "最後に、この面接を通じて、貴社の文化や今後のビジョンをもっと深く理解できれば幸いです。そして、もし幸運にも採用される機会があれば、全力で取り組み、貴社の発展に貢献したいと思います。貴社のご多忙の中で面接の機会をいただき、誠にありがとうございます。",
+            "以上です。よろしくお願いいたします。"
+
+
+            // "初めまして、簡単に自己紹介をさせていただきます。私は李秀傑と申します。33歳で、大連湾に住んでいます。今日は、貴社（きしゃ）の面接に参加させていただき、非常に光栄（こうえい）に思います。",
+            // "卒業以来、10年間、システム开発に従事（じゅうじ）しました。プログラマーとして、言語はドットネットで主に开発経験があります。SQL、HTML、js、css、Vueなど熟練度（じゅくれんど）も高いです。プログラムだけではなく、お客様と認識が合って、設計書の修正や製造や単体試験を経験しております。その他、業務を円滑（えんかつ）に進めるために、ツールにおいて、EXCELVBAを作った経験があります。",
+            // "最後に、この面接を通じて、貴社の文化や今後のビジョンをもっと深く理解できれば幸いです。そして、もし幸運にも採用される機会があれば、全力で取り組み、貴社の発展に貢献（こうけん）したいと思います。",
+            // "貴社のご多忙（たぼう）の中で面接の機会をいただき、誠にありがとうございます。",
+            // "以上です。よろしくお願いいたします。"
+
             // "Then I'll huff",
             // "and I'll puff ",
             // "and I'll blow your house down.",
